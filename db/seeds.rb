d14361d3053e8b2cc9c2c08c40453413673b4fb6
Mohamed Anwar <moanwar1997@gmail.com>
2019-04-28 09:58:44 +0400

    edititng the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,28 @@ survey.questions.create(
   [
     {
       body: 'Are you interested in being a host?',
-      type: Question.types[:yes_no]
+      type: Question.types[:yes_no],
+      audio_file: 'https://s3-ap-northeast-1.amazonaws.com/poodll-audioprocessing-out/CP/30/localhostuser/recordmp3online.com/poodll/poodllfile5cc4abde4a8281.mp3'
     },
     {
       body: 'What is your full name ?',
-      type: Question.types[:free]
+      type: Question.types[:free],
+      audio_file: 'https://s3-ap-northeast-1.amazonaws.com/poodll-audioprocessing-out/CP/30/localhostuser/recordmp3online.com/poodll/poodllfile5cc4ac15d81021.mp3'
     },
     {
       body: 'Which city you live in?',
-      type: Question.types[:free]
+      type: Question.types[:free],
+      audio_file: 'https://s3-ap-northeast-1.amazonaws.com/poodll-audioprocessing-out/CP/30/localhostuser/recordmp3online.com/poodll/poodllfile5cc4ac3402e3e1.mp3'
     },
     {
       body: 'Which District do you live in?',
-      type: Question.types[:free]
+      type: Question.types[:free],
+      audio_file: 'https://s3-ap-northeast-1.amazonaws.com/poodll-audioprocessing-out/CP/30/localhostuser/recordmp3online.com/poodll/poodllfile5cc4ac52cc33e1.mp3'
     },
     {
       body: 'upto how many students can you host?',
-      type: Question.types[:numeric]
+      type: Question.types[:numeric],
+      audio_file: 'https://s3-ap-northeast-1.amazonaws.com/poodll-audioprocessing-out/CP/30/localhostuser/recordmp3online.com/poodll/poodllfile5cc4ac7f4d8331.mp3'
     }
   ]
 )
